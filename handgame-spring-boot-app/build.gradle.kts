ec252722718aa78d4452b2e8a9a503770297e967
eunoiahyunseo <heart20021010@gmail.com>
2023-06-27 17:24:27 +0900

    first commit

---
+++ handgame-spring-boot-app/build.gradle.kts
@@ -0,0 +1,5 @@
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("com.fastcampus.springboot:handgame-spring-boot-starter:1.0-SNAPSHOT")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "thirdparty-module"
+
+include(
+    "handgame",
+    "handgame-spring-boot-autoconfigure",
+    "handgame-spring-boot-starter",
+    "handgame-spring-boot-app"
+)
